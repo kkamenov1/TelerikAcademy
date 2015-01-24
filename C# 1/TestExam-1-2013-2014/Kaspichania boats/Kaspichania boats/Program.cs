ee586d01e23663ea5fee19a75f1df297989b0e5e
kkamenov1 <kristiankamenov1@gmail.com>
2015-01-24 15:32:55 +0200

    Test Exam 2013/2014

---
+++ C# 1/TestExam-1-2013-2014/Kaspichania boats/Kaspichania boats/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaspichania_boats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write(new string('.', n));
+            Console.Write('*');
+            Console.WriteLine(new string('.', n));
+            for (int i = 0; i <= n - 2 ; i++)
+            {
+                Console.Write(new string('.', n - 1 - i));
+                Console.Write('*');
+                Console.Write(new string('.', i));
+                Console.Write('*');
+                Console.Write(new string('.', i));
+                Console.Write('*');
+                Console.WriteLine(new string('.', n - 1 - i));
+            }
+
+            Console.WriteLine(new string('*', 2 * n + 1));
+
+            for (int i = 0; i <= n/2; i++)
+            {
+                if (i == n / 2)
+                {
+                    Console.Write(new string('.', i + 1));
+                    Console.Write(new string('*', n));
+                    Console.WriteLine(new string('.', i + 1));
+                }
+                else
+                {
+                    Console.Write(new string('.', i + 1));
+                    Console.Write('*');
+                    Console.Write(new string('.', n - 2 - i));
+                    Console.Write('*');
+                    Console.Write(new string('.', n - 2 - i));
+                    Console.Write('*');
+                    Console.WriteLine(new string('.', i + 1));
+                }
+            }
+        }
+    }
+}
---
+++ C# 1/TestExam-1-2013-2014/NightmareOnCodeStreet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NightmareOnCodeStreet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int counter = 0;
+            int sum = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    counter++;
+                    sum += (int)(str[i] - '0');
+                }
+            }
+            Console.WriteLine("{0} {1}", counter, sum);
+
+           
+        }
+    }
+}
