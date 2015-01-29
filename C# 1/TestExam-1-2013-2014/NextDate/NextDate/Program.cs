28c7551930d4dae3f5edd5bbd1f76e84490a4fdb
kkamenov1 <kristiankamenov1@gmail.com>
2015-01-29 11:50:26 +0200

    added NextDate

---
+++ C# 1/TestExam-1-2013-2014/NextDate/NextDate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+
+            DateTime currentDate = new DateTime(year, month, day);
+            currentDate = currentDate.AddDays(1);
+            Console.WriteLine("{0}.{1}.{2}", currentDate.Day, currentDate.Month, currentDate.Year);
+        }
+    }
+}
